512b41062487bf3b4432df4654fcb6c5994e371d
Gyorgy Szaszko <adam.szaszko@gmail.com>
2018-07-11 14:06:48 +0200

    videostreaming

--- emulation/videostreaming/setTap.sh
+++ emulation/videostreaming/setTap.sh
@@ -9,6 +9,9 @@ ADDR=$2
 sudo ip tuntap add mode tap dev $DEV
 sudo ip addr add $ADDR/24 dev $DEV  # give it an ip
 sudo ip link set dev $DEV up  # bring the if up
+
+echo 2 | sudo tee /proc/sys/net/ipv4/conf/$DEV/rp_filter
+echo 1 | sudo tee /proc/sys/net/ipv4/conf/$DEV/accept_local
+
 ip route get $ADDR  # check that packets to 10.0.0.x are going through tun0
-echo ping 10.0.0.1  # leave this running in another shell to be able to see the effect of the next example
 fi
