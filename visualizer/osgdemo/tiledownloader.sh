4b0da64ac740c5fe406bde675813753c9b397525
Rudolf Hornig <rudi@omnetpp.org>
2018-08-16 14:09:47 +0200

    osgdemo: updated to the latest version

--- visualizer/osgdemo/tiledownloader.sh
+++ visualizer/osgdemo/tiledownloader.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-osgearth_package --tms mymap.earth  --bounds 16.4472 47.3925 16.4794 47.4080 --max-level 12 --out mymap --out-earth newmymap.earth
+osgearth_package --tms mymap_source.earth  --bounds 16.4472 47.3925 16.4794 47.4080 --max-level 19 --out mymap_v0301 --out-earth mymap_v0301.earth
